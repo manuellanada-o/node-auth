d376624ba724aeaec61ac2f464f99ba335ad0dc9
Manuel Lanada <manuellanada@outlook.com>
2025-02-25 17:19:02 +0800

    created interface for typing

--- api.ts
+++ api.ts
@@ -1,21 +1,38 @@
-// import express, { Request, Response } from 'express';
+// import express, { Request, Response } from 'express'
 
-// const app = express();
-// const port = 3000;
+// const app = express()
+// const port = 3000
 
 // app.get('/', (req: Request, res: Response) => {
-//     res.send('Hello Universe!');
-// });
+//     res.send('Hello Universe!')
+// })
 
 // app.get('/test', (req: Request, res: Response) => {
-//     res.send('test this shit');
-// });
+//     res.send('test this shit')
+// })
 
 // app.listen(port, () => {
-//     console.log(`Example app listening on port ${port}`);
-// });
+//     console.log(`Example app listening on port ${port}`)
+// })
 
-exports.handler = async (event: any, context: any) => {
+interface Event {
+    httpMethod: string
+    path: string
+    body: any
+}
+
+interface Context {
+    callbackWaitsForEmptyEventLoop: boolean
+    functionVersion: string
+    functionName: string
+    memoryLimitInMB: number
+    logGroupName: string
+    logStreamName: string
+    invokedFunctionArn: string
+    awsRequestId: string
+}
+
+exports.handler = async (event: Event, context: Context) => {
     console.log(`JSON.stringify(event)=${JSON.stringify(event)}`)
     console.log(`JSON.stringify(context)=${JSON.stringify(context)}`)
     if (event.httpMethod === 'GET') {
@@ -23,12 +40,12 @@ exports.handler = async (event: any, context: any) => {
             return {
                 statusCode: 200,
                 body: 'test',
-            };
+            }
         } catch (error) {
             return {
                 statusCode: 500,
                 body: JSON.stringify({ error: 'Failed to process GET request' }),
-            };
+            }
         }
     }
-};
+}
