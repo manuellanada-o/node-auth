7a9ab60cf90e9d52d919ab07da822a138056345b
Manuel Lanada <manuellanada@outlook.com>
2025-02-25 13:06:54 +0800

    test export handler

--- api.ts
+++ api.ts
@@ -1,16 +1,32 @@
-import express, { Request, Response } from 'express';
+// import express, { Request, Response } from 'express';
 
-const app = express();
-const port = 3000;
+// const app = express();
+// const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello Universe!');
-});
+// app.get('/', (req: Request, res: Response) => {
+//     res.send('Hello Universe!');
+// });
 
-app.get('/test', (req: Request, res: Response) => {
-    res.send('test this shit');
-});
+// app.get('/test', (req: Request, res: Response) => {
+//     res.send('test this shit');
+// });
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`);
+// });
+
+exports.handler = async (event: any, context: any) => {
+    if (event.httpMethod === 'GET') {
+        try {
+            return {
+                statusCode: 200,
+                body: 'test',
+            };
+        } catch (error) {
+            return {
+                statusCode: 500,
+                body: JSON.stringify({ error: 'Failed to process GET request' }),
+            };
+        }
+    }
+};
